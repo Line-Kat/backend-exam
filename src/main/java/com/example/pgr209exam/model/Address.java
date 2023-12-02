373da600ce8e93c235d64808681d34c88798d88e
L_Kat <l_katralen@hotmail.com>
2023-12-02 11:20:02 +0100

    Customer ManyToMany Address, one customer can have more than one address, and one address can belong to more than one customer

--- src/main/java/com/example/pgr209exam/model/Address.java
+++ src/main/java/com/example/pgr209exam/model/Address.java
@@ -1,10 +1,15 @@
 package com.example.pgr209exam.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,4 +22,9 @@ public class Address {
     @SequenceGenerator(name = "address-sequence-generator", sequenceName = "address-sequence", allocationSize = 1)
     @Column(name = "address_id")
     private Long addressId = 0L;
+
+    @ManyToMany(mappedBy = "addresses", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Customer> customers = new ArrayList<>();
+
 }
