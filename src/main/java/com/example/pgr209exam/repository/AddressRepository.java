2ee0d3ed3fba09059bf80ab5d47a1f042ca92fde
L_Kat <l_katralen@hotmail.com>
2023-12-05 09:15:38 +0100

    trying to implement pagination for the address domain

--- src/main/java/com/example/pgr209exam/repository/AddressRepository.java
+++ src/main/java/com/example/pgr209exam/repository/AddressRepository.java
@@ -1,9 +1,12 @@
 package com.example.pgr209exam.repository;
 
 import com.example.pgr209exam.model.Address;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    Page<Address> findAllAddresses(Pageable pageable);
 }
