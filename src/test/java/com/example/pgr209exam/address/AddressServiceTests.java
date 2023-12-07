c3419d90889e6e153629b1d54454cc87c84d84b2
L_Kat <l_katralen@hotmail.com>
2023-12-07 12:19:58 +0100

    I have implementet pagination for the address domain (service, controller). I have written tests for the class AddressService. The test for getAddresses fails and I think it has to do with the implementation of pagination (comments in the code over the test). I have written tests for getAddresses, getAddressById and createAddress in the class AddressController. The tests for getAddresses fails and I think it's the same problem as in the tests for AddressService

--- src/test/java/com/example/pgr209exam/address/AddressServiceTests.java
+++ src/test/java/com/example/pgr209exam/address/AddressServiceTests.java
@@ -1,78 +1,97 @@
 package com.example.pgr209exam.address;
 
 import com.example.pgr209exam.model.Address;
+import com.example.pgr209exam.repository.AddressRepository;
 import com.example.pgr209exam.service.AddressService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 //source for pagination: https://www.javaguides.net/2022/02/spring-data-jpa-pagination-and-sorting.html
 
+@SpringBootTest
 public class AddressServiceTests extends DatabaseTests{
+
+    @MockBean
+    private AddressRepository addressRepository;
 
     @Autowired
     private AddressService addressService;
 
-    @BeforeEach
-    public void setUp(){
-        addAddresses(new Address("Dronninggata"));
-    }
 
+
+    //TEST DOESN'T RUN BECAUSE 'addressPage' IS NULL. Why doesn't addressService.getAddresses(pageable) return anything?
+    //Is the implementation of pagination wrong!?
     @Test
     public void getAddresses_whenExisting_shouldReturn1() {
+
+        List<Address> addresses = List.of(new Address("Dronninggata"), new Address("Prinsessealleen"));
+        when(addressRepository.findAll()).thenReturn(addresses);
+
         Pageable pageable = PageRequest.of(0, 5);
         Page<Address> addressPage = addressService.getAddresses(pageable);
         long numberOfAddresses = addressPage.getTotalElements();
 
-        Assertions.assertEquals(1, numberOfAddresses);
+        Assertions.assertEquals(2, numberOfAddresses);
     }
 
     @Test
     public void getAddressById_whenExisting_shouldReturnAddress() {
-       Address address = addressService.getAddressById(1L);
+        Address address = new Address("Dronninggata");
+        when(addressRepository.findById(1L)).thenReturn(Optional.of(address));
 
-        Assertions.assertEquals("Dronninggata", address.getAddressName());
+        var actualAddress = addressService.getAddressById(1L);
+
+        Assertions.assertEquals(address, actualAddress);
     }
 
     @Test
     public void createAddress_addingNewAddress_shouldReturnAddress() {
         Address address = new Address("Prinsessealleen");
-        Address returnedAddress = addressService.createAddress(address);
+        when(addressRepository.save(address)).thenReturn(address);
 
-        Pageable pageable = PageRequest.of(0, 5);
-        Page<Address> addressPage = addressService.getAddresses(pageable);
-        List<Address> addresses = addressPage.getContent();
+        var returnedAddress = addressService.createAddress(address);
 
         Assertions.assertNotNull(returnedAddress);
         Assertions.assertEquals("Prinsessealleen", returnedAddress.getAddressName());
-        Assertions.assertEquals(2, addresses.size());
     }
 
     @Test
     public void updateAddress_updateExistingAddress_shouldReturnUpdatedAddress() {
-        Address address = addressService.getAddressById(1L);
+        Address address = new Address("Blåklokkestien");
+        when(addressRepository.save(address)).thenReturn(address);
+
+        var firstAddress = addressService.createAddress(address);
+        Assertions.assertEquals("Blåklokkestien", firstAddress.getAddressName());
+
         address.setAddressName("Rådhusgata");
-        Address updatedAddress = addressService.updateAddress(address);
+        when(addressRepository.save(address)).thenReturn(new Address("Rådhusgata"));
+        var updatedAddress = addressService.updateAddress(address);
 
         Assertions.assertEquals("Rådhusgata", updatedAddress.getAddressName());
     }
 
+    //End-to-end-test
     @Test
     public void deleteAddress_deleteExistingAddress_shouldReturnZero() {
+        addAddresses(new Address("Dronninggata"));
         addressService.deleteAddressById(1L);
 
-        Pageable pageable = PageRequest.of(0, 5);
-        Page<Address> addressPage = addressService.getAddresses(pageable);
-        Long numberOfAddresses = addressPage.getTotalElements();
+        Address address = addressService.getAddressById(1L);
 
-        Assertions.assertEquals(0, numberOfAddresses);
+        Assertions.assertNull(address);
     }
-
 }
